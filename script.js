8a4a0112256a1e6004dc5e3d250f844f144f3fe9
FierceThundr <51411635+FierceThundr@users.noreply.github.com>
2021-05-19 09:27:01 -0400

    Bug Fixes

--- script.js
+++ script.js
@@ -675,7 +675,7 @@ function keysupdate() {//Update key count display
   document.getElementById("keycount").innerHTML = '<b>Keys remaining: ' + Math.max((data.wiki.total[data.wiki.current] - data.wiki.keys[data.wiki.current]),0) + '</b>';
 }
 
-function iframeload() {
+function guideload() {
 	console.log("Load fire")
 	document.getElementById("preview").contentDocument.addEventListener('click', function(e) {sitepreview(-1)})
 }
