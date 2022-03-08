52b7371824a064ee4280aa037696f36cdce1bd9f
FierceThundr <51411635+FierceThundr@users.noreply.github.com>
2022-03-08 12:06:43 -0500

    Experimental Update 7

--- script.js
+++ script.js
@@ -741,7 +741,7 @@ function popwificlose() {
 
 function popnotes() {
 	if (data.popup.notes.active != 1) {
-		data.popup.wifi.active = 1
+		data.popup.notes.active = 1
 		data.popup.notes.reference = window.open("popup.html","Notes Mimic","height=500,width=500,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no")
 		data.popup.notes.reference.document.onunload = function() {
 			var win = window.opener
